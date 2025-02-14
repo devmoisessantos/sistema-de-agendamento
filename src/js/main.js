ef96dccf57282a95e7a6f9e84222f487f5943397
devmoisessantos <062moises@gmail.com>
2025-02-14 13:22:45 -0300

    adiciona suporte a CSS e favicon na aplicação Hair Day

---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+'use strict'
+
+// CSS
+import "../css/global.css"
+import "../css/form.css"
+import "../css/schedule.css"
--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,19 @@ module.exports = {
         liveReload: true,
     },
 
-    plugins: [new HTMLWebpackPlugin({
-        template: path.resolve(__dirname, 'index.html'),
-    })]
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: path.resolve(__dirname, 'index.html'),
+            favicon: path.resolve('src', 'assets', 'scissors.svg'),
+        }),
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    }
 }
