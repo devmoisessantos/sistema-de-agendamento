e89b9c134c5ba7ced98f086e72461a004a267f49
devmoisessantos <062moises@gmail.com>
2025-02-14 15:32:04 -0300

    adiciona suporte à biblioteca dayjs e configura a localidade para pt-br

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,12 @@
 'use strict'
 
+// importação  das configurações do dayjs
+import dayjs from 'dayjs';
+import ptBR from 'dayjs/locale/pt-br.js';
+
+dayjs.locale(ptBR);
+
+
 // CSS
 import "../css/global.css"
 import "../css/form.css"
