18b63a0c73ff033360fb37206be1b62677618b44
devmoisessantos <062moises@gmail.com>
2025-02-14 12:36:53 -0300

    adiciona arquivos iniciais da aplicação Hair Day, incluindo configuração do webpack, estrutura HTML e arquivos de configuração

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const { stat } = require('fs');
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    target: 'web',
+    mode: 'development',
+
+    entry: path.resolve(__dirname, 'src', 'js', 'main.js'),
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist'),
+        },
+        port: 9000,
+        open: true,
+        liveReload: true,
+    },
+
+    plugins: [new HTMLWebpackPlugin({
+        template: path.resolve(__dirname, 'index.html'),
+    })]
+}
