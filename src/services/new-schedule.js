9b2f335a955c699bcb294aa18a25d57cb6e4d1d1
devmoisessantos <062moises@gmail.com>
2025-02-16 14:55:25 -0300

    adiciona configuração da API e função para agendar novos horários

---
+++ src/services/new-schedule.js
@@ -0,0 +1,16 @@
+import { apiConfig } from './api-config.js'
+
+export async function scheduleNew({ id, name, when }) {
+    try {
+        await fetch(`${apiConfig.baseURL}/schedules`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ id, name, when })
+        })
+    } catch (error) {
+        console.log(error)
+        alert('Ocorreu um erro ao criar o agendamento. Tente novamente!', error)
+    }
+}
