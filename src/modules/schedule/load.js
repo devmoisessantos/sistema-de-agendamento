8ff279f7b53c6b78d9d18ba7e74c1ce17ba1f862
devmoisessantos <062moises@gmail.com>
2025-02-17 00:09:58 -0300

    atualiza a função de carregamento de agendamentos para buscar e exibir agendamentos diários

--- src/modules/schedule/load.js
+++ src/modules/schedule/load.js
@@ -1,12 +1,18 @@
+import { findSchedule } from '../../services/find-schedule.js'
+import { showSchedule } from '../schedule/show.js'
 import { loadHours } from '../form/hours-load.js'
 
 // selecionando o input de data
 const selectedDate = document.getElementById('date')
 
-export function loadSchedule() {
+export async function loadSchedule() {
     // pegando o input de data
     const date = selectedDate.value
 
+    const dailySchedules = await findSchedule({ date })
+
+    showSchedule({ dailySchedules })
+
     // carregando as horas
-    loadHours({ date })
+    loadHours({ date, dailySchedules })
 }
